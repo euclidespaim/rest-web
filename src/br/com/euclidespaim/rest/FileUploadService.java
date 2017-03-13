c4b38c35a50ecde00859a69daef89d2b9a8c7f37
eucidespaim <euclidespaim@gmail.com>
2017-03-13 18:01:38 -0300

    Upload de arquivos p/ nuvem FUNCIONANDO!

---
+++ src/br/com/euclidespaim/rest/FileUploadService.java
@@ -0,0 +1,104 @@
+package br.com.euclidespaim.rest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+@Path("/upload")
+public class FileUploadService {
+	/** The path to the folder where we want to store the uploaded files */
+	private static final String UPLOAD_FOLDER = "C:\\Users\\Kid\\Desktop\\up/";
+	public FileUploadService() {
+	}
+	@Context
+	private UriInfo context;
+	/**
+	 * Returns text response to caller containing current time-stamp
+	 * 
+	 * @return error response in case of missing parameters an internal
+	 *         exception or success response if file has been stored
+	 *         successfully
+	 */
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("uploadedFile") InputStream uploadedInputStream,
+			@FormDataParam("uploadedFile") FormDataContentDisposition fileDetail) {
+		// check if all form parameters are provided
+		if (uploadedInputStream == null || fileDetail == null)
+			return Response.status(400).entity("Invalid form data").build();
+		// create our destination folder, if it not exists
+		try {
+			createFolderIfNotExists(UPLOAD_FOLDER);
+		} catch (SecurityException se) {
+			return Response.status(500)
+					.entity("Can not create destination folder on server")
+					.build();
+		}
+		String uploadedFileLocation = UPLOAD_FOLDER + fileDetail.getFileName();
+		try {
+			saveToFile(uploadedInputStream, uploadedFileLocation);
+		} catch (IOException e) {
+			return Response.status(500).entity("Can not save file").build();
+		}
+		return Response.status(200)
+				.entity("File saved to " + uploadedFileLocation).build();
+	}
+	/**
+	 * Utility method to save InputStream data to target location/file
+	 * 
+	 * @param inStream
+	 *            - InputStream to be saved
+	 * @param target
+	 *            - full path to destination file
+	 */
+	private void saveToFile(InputStream inStream, String target)
+			throws IOException {
+		OutputStream out = null;
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		out = new FileOutputStream(new File(target));
+		while ((read = inStream.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
+		}
+		out.flush();
+		out.close();
+	}
+	/**
+	 * Creates a folder to desired location if it not already exists
+	 * 
+	 * @param dirName
+	 *            - full path to the folder
+	 * @throws SecurityException
+	 *             - in case you don't have permission to create the folder
+	 */
+	private void createFolderIfNotExists(String dirName)
+			throws SecurityException {
+		File theDir = new File(dirName);
+		if (!theDir.exists()) {
+			theDir.mkdir();
+		}
+	}
+}
