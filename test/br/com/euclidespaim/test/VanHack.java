209a67daeb1afc05a2fe586e5166832b6243d216
eucidespaim <euclidespaim@gmail.com>
2017-04-10 07:42:00 -0300

    Maratona de progrmacao hackthon

---
+++ test/br/com/euclidespaim/test/VanHack.java
@@ -0,0 +1,51 @@
+package br.com.euclidespaim.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class VanHack {
+
+	public static void main(String[] args) {
+		JSONObject jsonObject;
+		//Cria o parse de tratamento
+		JSONParser parser = new JSONParser();
+		
+		//Variaveis que irao armazenar os dados do arquivo JSON
+		String id;
+		String title;
+		String total_votes;
+		String intro;
+		String body;
+		String tasks;
+
+		try {
+			//Salva no objeto JSONObject o que o parse tratou do arquivo
+			jsonObject = (JSONObject) parser.parse(new FileReader("C:/Users/Kid/workspace/nodeJS/vanhack.json"));
+			
+			//Salva nas variaveis os dados retirados do arquivo
+			id = (String) jsonObject.get("id");
+			title = (String) jsonObject.get("title");
+			total_votes = (String) jsonObject.get("total_votes");
+			intro = (String) jsonObject.get("intro");
+
+			System.out.printf(
+					"Data: %s\ntitle: %s\nid: %s\nintro: %s\n",
+					id, title, intro, total_votes);
+		} 
+		//Trata as exceptions que podem ser lançadas no decorrer do processo
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
